63c10851a069dace8f3c402a5253060788e3fcfb
najmi9 <imadnajmi9@gmail.com>
2022-09-23 10:38:24 +0100

    add nginx

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -1,15 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AppController extends AbstractController
 {
-    #[Route('/app', name: 'app_app')]
-    public function index(): Response
+    #[Route('/', name: 'app_app', methods:['GET'])]
+    public function index(Request $request): Response
     {
         return $this->render('app/index.html.twig', [
             'controller_name' => 'AppController',
